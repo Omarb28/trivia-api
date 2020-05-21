d40f7c972016a42636aa072e93ab7cbc876cd78b
Omarb28 <omarb28@gmail.com>
2020-05-21 20:36:05 +0000

    Implemented test for retrieving questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.db_username = os.environ['DB_USER']
+        self.db_password = os.environ['DB_PSWD']
+        self.database_path = "postgres://{}:{}@{}/{}".format(self.db_username, self.db_password, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +35,24 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+      res = self.client().get('/questions')
+      data = json.loads(res.data.decode('utf-8'))
+
+      self.assertEqual(res.status_code, 200)
+      self.assertEqual(data['success'], True)
+      self.assertTrue(data['total_questions'])
+      self.assertTrue(len(data['questions']))
+      self.assertTrue(data['categories'])
+      self.assertTrue(data['current_category'])
+      
+    def test_404_sent_requesting_beyond_valid_page(self):
+      res = self.client().get('/questions?page=1000')
+      data = json.loads(res.data.decode('utf-8'))
+
+      self.assertEqual(res.status_code, 404)
+      self.assertEqual(data['success'], False)
+      self.assertEqual(data['message'], 'Not Found')
 
 
 # Make the tests conveniently executable
