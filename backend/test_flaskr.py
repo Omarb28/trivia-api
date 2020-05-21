7983884336d08f77bdf8ca0ece8ce53e7bb8df5b
Omarb28 <omarb28@gmail.com>
2020-05-21 20:40:48 +0000

    Implemented test for creating questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -20,6 +20,13 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgres://{}:{}@{}/{}".format(self.db_username, self.db_password, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.new_question = {
+            'question_title': 'What is love?',
+            'answer': 'Baby don\'t hurt me',
+            'difficulty': 5,
+            'category': 1
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -53,6 +60,23 @@ class TriviaTestCase(unittest.TestCase):
       self.assertEqual(res.status_code, 404)
       self.assertEqual(data['success'], False)
       self.assertEqual(data['message'], 'Not Found')
+
+    def test_create_question(self):
+      res = self.client().post('/questions', json=self.new_question)
+      data = json.loads(res.data.decode('utf-8'))
+
+      self.assertEqual(res.status_code, 200)
+      self.assertEqual(data['success'], True)
+      self.assertTrue(data['created'])
+      #self.assertTrue(len(data['questions']))
+
+    def test_405_if_question_creation_not_allowed(self):
+      res = self.client().post('/questions/1000', json=self.new_question)
+      data = json.loads(res.data.decode('utf-8'))
+      
+      self.assertEqual(res.status_code, 405)
+      self.assertEqual(data['success'], False)
+      self.assertEqual(data['message'], 'Method Not Allowed')
 
 
 # Make the tests conveniently executable
